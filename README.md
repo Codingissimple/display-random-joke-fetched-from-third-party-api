a07cbeabe42c2506f8c01fdbf1abf7a44061c74e
codingissimple <ethananderson945@gmail.com>
2023-12-23 17:08:27 +0530

    random joke generator app

---
+++ README.md
@@ -0,0 +1,23 @@
+# Random Joke Generator
+
+[Author Website](https://codingissimple.com)
+
+[Learn asynchronous programming in JavaScript to build this app](https://codingissimple.com/how-to-return-response-from-an-asynchronous-call-in-javascript/)
+
+This simple web application fetches a random joke from a third-party API endpoint: [https://v2.jokeapi.dev/joke/Any](https://v2.jokeapi.dev/joke/Any).
+
+## Instructions
+
+1. Clone or download the repository to your local machine.
+
+2. Open the `index.html` file in a web browser.
+
+3. The page will display a random joke fetched from the API.
+
+4. Click the "Fetch Random Joke" button to fetch a new random joke.
+
+## Disclaimer
+
+This application is built for educational purposes. Jokes are not filtered, and we are not responsible if any joke offends anyone. The joke is randomly fetched from the API.
+
+Feel free to explore and enjoy reading the random jokes!
---
+++ script.js
@@ -0,0 +1,40 @@
+function fetchRandomJoke() {
+  return new Promise((resolve, reject) => {
+    fetch("https://v2.jokeapi.dev/joke/Any")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Network response was not ok");
+        }
+      })
+      .then((data) => resolve(data))
+      .catch((error) => reject(error));
+  });
+}
+
+function displayJoke(joke) {
+  document.getElementById("joke-setup").innerText = joke.setup;
+  document.getElementById("joke-delivery").innerText = joke.delivery || ""; // Display delivery only if available
+}
+
+document.getElementById("fetch-btn").addEventListener("click", () => {
+  fetchRandomJoke()
+    .then((joke) => {
+      displayJoke(joke);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
+// Fetch and display a joke on page load
+document.addEventListener("DOMContentLoaded", () => {
+  fetchRandomJoke()
+    .then((joke) => {
+      displayJoke(joke);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
